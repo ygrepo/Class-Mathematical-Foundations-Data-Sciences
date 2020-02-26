b0b92e75b60dcab8b532a95fdee703bc02bd1e77
Yves Greatti <yvgrotti@gmail.com>
2020-02-25 23:48:46 -0500

    Problem4 coding complete

--- hw/hw3/code/regression.py
+++ hw/hw3/code/regression.py
@@ -5,6 +5,29 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 
+def compute_error(less_a, less_a_pred, greater_a, greater_a_pred):
+    less_a_error = mean_squared_error(less_a, less_a_pred)
+    # less_a_train_sample_weights = np.array([1. / rain_train_less_a_pred.shape[0]])
+    # less_a_train_sample_weights = np.tile(less_a_train_sample_weights, rain_train_less_a_pred.shape[0])
+
+    greater_a_error = mean_squared_error(greater_a, greater_a_pred)
+
+    n_samples_less_a = less_a.shape[0]
+    n_samples_greater_a = greater_a.shape[0]
+    error = n_samples_less_a * less_a_error + n_samples_greater_a * greater_a_error
+    error = error / (n_samples_less_a + n_samples_greater_a)
+    print("{},{}, {}".format(less_a_error, greater_a_error, error))
+    return error
+
+    # greater_a_train_sample_weights = np.array([1. / rain_train_greater_a_pred.shape[0]])
+    # greater_a_train_sample_weights = np.tile(greater_a_train_sample_weights, rain_train_greater_a_pred.shape[0])
+    #
+    # stacked_rain_train = np.hstack((rain_train_less_a, rain_train_greater_a))
+    # stacked_rain_train_pred = np.hstack((rain_train_less_a_pred, rain_train_greater_a_pred))
+    # stacked_train_sample_weights = np.hstack((less_a_train_sample_weights, greater_a_train_sample_weights))
+    # train_error = mean_squared_error(stacked_rain_train, stacked_rain_train_pred, stacked_train_sample_weights)
+
+
 def split_and_plot(a, max_temp_train, rain_train, max_temp_val, rain_val, grid):
     grid_less_a = grid[grid < a].reshape(-1, 1)
     grid_greater_a = grid[grid >= a].reshape(-1, 1)
@@ -49,55 +72,19 @@ def split_and_plot(a, max_temp_train, rain_train, max_temp_val, rain_val, grid):
 
     # use a relevant error metric below. make sure you normalize correctly to take into account number of datapoints.
     rain_train_less_a_pred = reg_1.predict(max_temp_train_less_a)
-    train_less_a_error = mean_squared_error(rain_train_less_a, rain_train_less_a_pred)
-    #less_a_train_sample_weights = np.array([1. / rain_train_less_a_pred.shape[0]])
-    #less_a_train_sample_weights = np.tile(less_a_train_sample_weights, rain_train_less_a_pred.shape[0])
-
     rain_train_greater_a_pred = reg_2.predict(max_temp_train_greater_a)
-    train_greater_a_error = mean_squared_error(rain_train_greater_a, rain_train_greater_a_pred)
-
-    train_n_samples_less_a = rain_train_less_a.shape[0]
-    train_sample_greater_a = rain_train_greater_a.shape[0]
-    train_error = train_n_samples_less_a * train_less_a_error + train_sample_greater_a * train_greater_a_error
-    train_error = train_error / (train_n_samples_less_a + train_sample_greater_a)
-    print("Train:{},{}, {}".format(train_less_a_error, train_greater_a_error, train_error))
-
-    # greater_a_train_sample_weights = np.array([1. / rain_train_greater_a_pred.shape[0]])
-    # greater_a_train_sample_weights = np.tile(greater_a_train_sample_weights, rain_train_greater_a_pred.shape[0])
-    #
-    # stacked_rain_train = np.hstack((rain_train_less_a, rain_train_greater_a))
-    # stacked_rain_train_pred = np.hstack((rain_train_less_a_pred, rain_train_greater_a_pred))
-    # stacked_train_sample_weights = np.hstack((less_a_train_sample_weights, greater_a_train_sample_weights))
-    # train_error = mean_squared_error(stacked_rain_train, stacked_rain_train_pred, stacked_train_sample_weights)
+    train_error = compute_error(rain_train_less_a, rain_train_less_a_pred, rain_train_greater_a,
+                                rain_train_greater_a_pred)
 
     # And we split the same way the validation dataset related to the temperatures
     max_temp_val_less_a = max_temp_val[np.where(max_temp_val < a)].reshape(-1, 1)
     rain_val_less_a = rain_val[np.where(max_temp_val < a)]
     rain_val_less_a_pred = reg_1.predict(max_temp_val_less_a)
-    val_less_a_error = mean_squared_error(rain_val_less_a, rain_val_less_a_pred)
-
-
-    less_a_val_sample_weights = np.array([1. / rain_val_less_a_pred.shape[0]])
-    less_a_val_sample_weights = np.tile(less_a_val_sample_weights, rain_val_less_a_pred.shape[0])
 
     max_temp_val_greater_a = max_temp_val[np.where(max_temp_val >= a)].reshape(-1, 1)
     rain_val_greater_a = rain_val[np.where(max_temp_val >= a)]
     rain_val_greater_a_pred = reg_2.predict(max_temp_val_greater_a)
-    val_greater_a_error = mean_squared_error(rain_val_greater_a, rain_val_greater_a_pred)
-
-    val_n_samples_less_a = rain_val_less_a.shape[0]
-    val_n_samples_greater_a = rain_val_greater_a.shape[0]
-    val_error = val_n_samples_less_a * val_less_a_error + val_n_samples_greater_a * val_greater_a_error
-    val_error = val_error / (val_n_samples_less_a + val_n_samples_greater_a)
-    print("val:{},{}, {}".format(val_less_a_error, val_greater_a_error, val_error))
-
-    # greater_a_val_sample_weights = np.array([1. / rain_val_greater_a_pred.shape[0]])
-    # greater_a_val_sample_weights = np.tile(greater_a_val_sample_weights, rain_val_greater_a_pred.shape[0])
-    #
-    # stacked_rain_val = np.hstack((rain_val_less_a, rain_val_greater_a))
-    # stacked_rain_val_pred = np.hstack((rain_val_less_a_pred, rain_val_greater_a_pred))
-    # stacked_val_sample_weights = np.hstack((less_a_val_sample_weights, greater_a_val_sample_weights))
-    # val_error = mean_squared_error(stacked_rain_val, stacked_rain_val_pred, stacked_val_sample_weights)
+    val_error = compute_error(rain_val_less_a, rain_val_less_a_pred, rain_val_greater_a, rain_val_greater_a_pred)
 
     # val_error =  # error of your fit on validation data points
     # train_error =  # error of your fit on your train data points
@@ -105,7 +92,7 @@ def split_and_plot(a, max_temp_train, rain_train, max_temp_val, rain_val, grid):
         'a = ' + str(a) + ' val error: ' + str(round(val_error, 3)) + ' train error: ' + str(round(train_error, 3)),
         fontsize=18)
     plt.savefig('a=' + str(a) + '.pdf')
-    #plt.show()
+    # plt.show()
 
 
 def main():
@@ -128,12 +115,12 @@ def main():
     grid = np.arange(0, max_val + 1, width_bin)
     grid = grid.reshape(-1, 1)
 
-    #for a in [4]:
+    # for a in [4]:
     for a in np.arange(4, 25, 4):
         split_and_plot(a, max_temp_train, rain_train, max_temp_val, rain_val, grid)
 
     ### selecting the best model according to val accuracy
-    a = 4  # best value of a from grid search above
+    a = 8  # best value of a from grid search above
 
     grid_less_a = grid[grid < a].reshape(-1, 1)
     grid_greater_a = grid[grid >= a].reshape(-1, 1)
@@ -172,10 +159,14 @@ def main():
     plt.gcf().subplots_adjust(bottom=0.15)
     plt.gcf().subplots_adjust(left=0.15)
 
+    a_error = compute_error(grid_less_a, linear_fit_rain_less_a, grid_greater_a, linear_fit_rain_greater_a)
+    lr_error = mean_squared_error(grid, linear_fit_rain)
+
     # a_error =  # error of your best model
     # lr_error =  # error of regular linear regression
-    # plt.title('a=' + str(a) + '_error: ' + str(round(a_error, 3)) + ' LR_error: ' + str(round(lr_error, 3)),
-    #           fontsize=18)
+
+    plt.title('a=' + str(a) + '_error: ' + str(round(a_error, 3)) + ' LR_error: ' + str(round(lr_error, 3)),
+              fontsize=18)
     plt.savefig('test_comparison.pdf')
 
 
